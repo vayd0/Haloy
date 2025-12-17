188c83fef663739b55aa0cdc76d2a46a57c41fd7
Simon Szczesny <simon_szczesny@ens.univ-artois.fr>
2025-12-17 12:59:51 +0100

    Routes mises à jour : route d'accueil utilisant le contrôleur et route pour afficher le détail d'un article, #5

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
+use App\Http\Controllers\AccueilController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return view('welcome');
-})->name("accueil");
 
 Route::get('/contact', function () {
     return view('contact');
@@ -20,4 +17,5 @@ Route::get('/home', function () {
     return view('home');
 })->name("home");
 
-
+Route::get('/', [AccueilController::class, 'index'])->name("accueil");
+Route::get('/article/{id}', [App\Http\Controllers\ArticleController::class, 'show'])->name("article.show");
