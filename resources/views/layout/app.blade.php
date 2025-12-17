c1deb57944cef26e94db1fdb3c82d376f5e2cbe6
Luc BAÏR--NAPIERALA <luc_bair--napierala@ens.univ-artois.fr>
2025-12-17 19:27:17 +0100

    Suppression de partial.css

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,7 +6,7 @@
     <title>{{isset($title) ? $title : "Page en cours"}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js', 'resources/css/partials.css'])
+    @vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
