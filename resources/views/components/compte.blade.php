a0bab990ca67bdd7cf3fa3c240364e7368826094
vayd0 <kiiroxyz@gmail.com>
2025-12-18 08:34:48 +0100

    Modifications welcome, article-card

---
+++ resources/views/components/compte.blade.php
@@ -0,0 +1,21 @@
+<div class="relative">
+    <button type="button" class="flex items-center gap-2 px-4 py-2 bg-white/10 rounded hover:bg-white/20 transition"
+        id="dropdown-toggle">
+        <div class="flex justify-between items-center gap-2">
+            <x-avatar />
+            {{Auth::user()->name}}
+        </div>
+        <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+            <path d="M19 9l-7 7-7-7" />
+        </svg>
+    </button>
+    <div id="dropdown-menu"
+        class="absolute glass-morph left-0 mt-2 w-40 rounded shadow-lg py-2 opacity-0 pointer-events-none transition-all z-50" style="border-radius:10px;">
+        <a href="{{ "profile" }}" class="block px-4 py-2 hover:bg-gray-100">Profil</a>
+        <a href="{{ route('logout') }}" onclick="document.getElementById('logout').submit(); return false;"
+            class="block px-4 py-2 hover:bg-gray-100 text-redc">Déconnexion</a>
+        <form id="logout" action="{{ route('logout') }}" method="post" class="hidden">
+            @csrf
+        </form>
+    </div>
+</div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,12 +6,12 @@
     <title>{{isset($title) ? $title : "Page en cours"}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js'])
+    @vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js', 'resources/js/dropdown.js', 'resources/js/navbar.js'])
 </head>
 
 <body>
     @include("partials.navbar")
-
+    @include("components.background")
     <main>
         @yield("contenu")
     </main>
