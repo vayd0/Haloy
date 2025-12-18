4a0e9f67919180557713448c13e88d7600161b15
Luc BAÏR--NAPIERALA <luc_bair--napierala@ens.univ-artois.fr>
2025-12-18 14:17:28 +0100

    Ajout des seeders pour les notifications

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -25,7 +25,7 @@ class UsersSeeder extends Seeder
         $access = Accessibilite::firstOrCreate(['texte' => 'Facile'], ['texte' => 'Facile']);
         $concl = Conclusion::firstOrCreate(['texte' => 'Ouverte'], ['texte' => 'Ouverte']);
 
-        // 1. CRÉATION DE "LES INROCKS" (Ton compte de test)
+        // 1. CRÉATION DE "LES INROCKS"
         $inrocks = User::firstOrCreate(
             ['email' => 'inrocks@gmail.com'],
             [
@@ -40,7 +40,7 @@ class UsersSeeder extends Seeder
         // 2. CRÉATION DES USERS LAMBDA
         // On crée user1 spécifiquement pour le scénario
         $user1 = User::factory()->create([
-            'name' => 'John Lennon', // On lui donne un nom cool
+            'name' => 'John Lennon',
             'email' => "user1@gmail.com",
             'email_verified_at' => now(),
             'password' => $hash,
@@ -60,28 +60,34 @@ class UsersSeeder extends Seeder
         // A. Inrocks s'abonne à John Lennon (user1)
         $inrocks->suivis()->attach($user1->id);
 
-        // B. John Lennon publie 5 articles d'un coup !
+        // B. John Lennon publie 5 articles d'un coup
         $this->command->info('Génération des 5 articles et envoi des notifs...');
 
         for ($j = 1; $j <= 5; $j++) {
+            $dateFictive = $faker->dateTimeBetween('-1 year', '-1 week');
+
             $article = Article::create([
-                'titre' => "Article Exclusif N°" . $j, // Titres différents
+                'titre' => "Article Exclusif N°" . $j,
                 'resume' => "Voici le résumé de l'actualité numéro " . $j,
                 'texte' => $faker->paragraph(3),
-                'image' => 'articles/default.jpg',
+
+                // 2. IMAGE ALEATOIRE : On pioche entre articles1.png et articles50.png
+                'image' => 'images/articles' . rand(1, 50) . '.png',
+
                 'media' => 'articles/default.mp3',
                 'rythme_id' => $rythme->id,
                 'accessibilite_id' => $access->id,
                 'conclusion_id' => $concl->id,
-                'user_id' => $user1->id, // C'est user1 qui écrit
+                'user_id' => $user1->id,
                 'en_ligne' => true,
-            ]);
 
-            // C. Envoi de la notification pour chaque article
-            // Comme Inrocks suit user1, on notifie Inrocks
+                // 3. ON FORCE LA DATE
+                'created_at' => $dateFictive,
+                'updated_at' => $dateFictive,
+            ]);
             Notification::send($inrocks, new NewArticlePublished($article));
         }
 
-        $this->command->info('TERMINE : Connecte-toi sur inrocks@gmail.com pour voir les 5 notifs !');
+        $this->command->info('TERMINE : inrocks@gmail.com a 5 notifs !');
     }
 }
